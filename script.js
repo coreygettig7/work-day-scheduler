2930727ba8733f78d470314811ddcda380e9665c
Corey Gettig <coreygettig7@gmail.com>
2022-01-16 17:09:42 -0500

    added divs for each hour slot and used classes provided in starter code to style them, also added function in java to color code the divs based on past, present, future

--- script.js
+++ script.js
@@ -5,3 +5,16 @@ function todaysDate(now=moment()) {
 
 todaysDate();
 
+// adds colors to time slots depending on past, present, and future
+var checkHour = new Date(); // initiates current date and time
+var currentHour = checkHour.getHours(); // returns 0-23 on a 24 hour clock
+
+for (var i = 8; i < 18; i++) {
+    if (i < currentHour) {
+        document.getElementById(i.toString()).classList.add('past');
+    } else if (i === currentHour) {
+        document.getElementById(i.toString()).classList.add('present');
+    } else if (i > currentHour) {
+        document.getElementById(i.toString()).classList.add('future');
+    }
+};
